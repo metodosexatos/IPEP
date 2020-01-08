b4d364176fab622ceda7a012bbd6b689d99a457e
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2020-01-08 12:09:21 -0300

    Add files via upload

---
+++ 02.First_Rules-WhitespaceFormatting.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Whitespace Formatting
+
+# In[1]:
+
+
+for i in [2]:
+    print(i)              # first line in "for i" block
+    for j in [15]:
+        print(j)          # first line in "for j" block
+        print(i + j)      # last line in "for j" block
+    print(i + 1)          # last line in "for i" block
+print('done looping')
+
+
+# In[2]:
+
+
+for i in [2, 5]:
+    print(i)
+    for j in [15, 28]:
+        print(j)
+        print(i + j)
+    print(i + 1)
+print('done looping')
+
+
+# In[3]:
+
+
+for i in [2, 5]:
+    print(i)
+    for j in [15, 28]:
+        print(j)
+        print(i + j)
+    print(i + 1)
+    print('done looping')
+
+
+# In[ ]:
+
+
+
+
