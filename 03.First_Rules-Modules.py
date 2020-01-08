d113b118ecb39bf433d42595fb673759e8518acc
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2020-01-08 20:17:35 -0300

    Add files via upload

---
+++ 03.First_Rules-Modules.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Using a Module
+
+# $\sqrt{81}\$
+
+# In[1]:
+
+
+81**0.5
+
+
+# In[2]:
+
+
+sqrt(81)
+
+
+# In[3]:
+
+
+import math
+math.sqrt(81)
+
+
+# In[5]:
+
+
+from math import *
+sqrt(81)
+
+
+# In[6]:
+
+
+from math import sqrt
+sqrt(81)
+
+
+# In[7]:
+
+
+import math as metodosexatos
+metodosexatos.sqrt(81)
+
