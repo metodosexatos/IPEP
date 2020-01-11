c3ef39ace056866bb59f6a973958ee56c7cc7396
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2020-01-11 19:47:03 -0300

    Add files via upload

---
+++ 10.DataStructures_Numeric.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Different Kinds of Numbers
+
+# In[ ]:
+
+
+# int:     Integers (ex.: 3)
+# float:   Floating point numbers (ex.: -1.34)
+# complex: Complex numbers (1 + 2j)
+
+
+# In[1]:
+
+
+type(3)
+
+
+# In[2]:
+
+
+type(3.0)
+
+
+# In[3]:
+
+
+type(3.)
+
+
+# In[4]:
+
+
+type(3.5)
+
+
+# In[5]:
+
+
+type(1/3)
+
+
+# In[6]:
+
+
+type(3j)
+
+
+# In[7]:
+
+
+type(2+3j)
+
+
+# In[8]:
+
+
+x = 3
+
+
+# In[9]:
+
+
+type(x)
+
+
+# In[10]:
+
+
+y = 9**(0.5)
+print(y)
+
+
+# In[11]:
+
+
+type(y)
+
