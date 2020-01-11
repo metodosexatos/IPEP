8a4a4b84adb926ac32cce211c5112ef907616bb2
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2020-01-11 20:26:05 -0300

    Add files via upload

---
+++ 11.DataStructures_Manipulating-Number-Types.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Manipulating Number Types
+
+# In[1]:
+
+
+# int:     Integers
+# float:   Floating point numbers
+# complex: Complex numbers
+
+
+# In[2]:
+
+
+a = 0
+type(a)
+
+
+# In[3]:
+
+
+complex(a)
+
+
+# In[4]:
+
+
+float(a)
+
+
+# In[5]:
+
+
+int(4.0)
+
+
+# In[6]:
+
+
+int(4.35)
+
+
+# In[7]:
+
+
+a = complex(a)
+type(a)
+
+
+# In[8]:
+
+
+int(3j)
+
