f276d9814ea945b8a0c681f12892a7f58e180d64
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2020-01-14 16:05:50 -0300

    Add files via upload

---
+++ 17.DataStructures_List-Functions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # List Functions
+
+# In[1]:
+
+
+prices = [3, 41, 12, 9, 74, 15]
+
+
+# In[2]:
+
+
+print(max(prices))
+
+
+# In[3]:
+
+
+print(min(prices))
+
+
+# In[4]:
+
+
+print(max(prices)-min(prices))
+
+
+# In[5]:
+
+
+print(sum(prices))
+
+
+# In[6]:
+
+
+print(sum(prices)/len(prices))
+
